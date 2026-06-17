54e8d8f3b74b534cb9e190cbd4dd521515dd73e5
Fahmyelmatry <fahmyelmatry2030@gmail.com>
2026-06-17 05:53:35 +0300

    add HTTP to HTTPS redirects in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "http",
+          },
+        ],
+        destination: "https://doctornafsyonline.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
